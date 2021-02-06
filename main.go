ae4e1e6dc73046ae3d03142efe19eec56de4784c
Tadeu <tadeu.tupiz@gmail.com>
2021-02-06 15:04:50 -0300

    routes with request

--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Someone visit our site")
+	fmt.Println("URL" + r.URL.Path)
+
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get it touch, please send an email to <a href=\"mailto:tadeu.tupiz@gmail.com\">tadeu.tupiz@gmail.com</a>")
+	}
 }
 
 func main() {
