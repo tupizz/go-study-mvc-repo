81ab44e497761150ee49a97cff5633623944d26e
Tadeu <tadeu.tupiz@gmail.com>
2021-02-06 15:26:41 -0300

    🦍 not found with gorilla

--- main.go
+++ main.go
@@ -7,21 +7,28 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Someone visit our handleHome")
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
 }
 
-func handleContact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Someone visit our handleContact")
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get it touch, please send an email to <a href=\"mailto:tadeu.tupiz@gmail.com\">tadeu.tupiz@gmail.com</a>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1 style=\"color: red;\">Not found :(</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleHome)
-	r.HandleFunc("/contact", handleContact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
